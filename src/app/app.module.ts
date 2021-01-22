0b308dc5d3c5a9141a65635be60e0b871fc81d67
japc <japc.grafico@gmail.com>
2021-01-22 18:14:32 +0100

    Custom modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-// Angular Material
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
+
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
@@ -15,10 +14,10 @@ import {MatButtonModule} from '@angular/material/button';
   ],
   imports: [
     BrowserModule,
+
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatButtonModule
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
