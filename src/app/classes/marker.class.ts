8e5f40a08332ce7cf4def8bc28792cd7e9086bf9
japc <japc.grafico@gmail.com>
2021-01-25 10:52:32 +0100

    Creating markers dynamically

---
+++ src/app/classes/marker.class.ts
@@ -0,0 +1,10 @@
+
+export class Marker {
+    public title = 'Without title';
+    public desc = 'Description';
+
+    constructor(
+        public lat: number,
+        public lng: number
+    ) { }
+}
--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Marker } from 'src/app/classes/marker.class';
 
 @Component({
   selector: 'app-map',
@@ -7,11 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
+  markers: Marker[] = [];
+
   lat = 51.678418;
   lng = 7.809007;
 
-  constructor() { }
+  constructor() {
+    const newMarker = new Marker(51.678418 , 7.809007);
+    this.markers.push(newMarker);
+  }
 
   ngOnInit(): void {
+  }
+
+  addMarker(e: any): void {
+    const coords: { lat: number, lng: number} = e.coords;
+    // console.log(e);
+    const newMarker = new Marker(coords.lat, coords.lng);
+    this.markers.push(newMarker);
   }
 }
